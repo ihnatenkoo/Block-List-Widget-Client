41bcb2bbb6a46d5c765bb5707267917d1f75efb3
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-06-13 11:13:03 +0300

    Update next config to avoid cors error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites() {
+    return [
+      { source: '/api/:path*', destination: 'http://localhost:3001/:path*' },
+    ];
+  },
 };
 
 export default nextConfig;
