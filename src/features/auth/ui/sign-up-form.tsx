887a66a286ef534f2f49d107c3cc693c3aaed01a
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-06-20 12:52:10 +0300

    Implement sign-up functionality

--- src/features/auth/ui/sign-up-form.tsx
+++ src/features/auth/ui/sign-up-form.tsx
@@ -1,12 +1,47 @@
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import { UiButton } from '@/shared/ui/ui-button';
 import { UiTextField } from '@/shared/ui/ui-text-filed';
+import { authControllerSignUp } from '@/shared/api/generated';
+import { ROUTES } from '@/shared/constants/routes';
 
 export const SignUpForm = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<{
+    email: string;
+    password: string;
+  }>();
+
+  const signUpMutation = useMutation({
+    mutationFn: authControllerSignUp,
+    onSuccess() {
+      router.push(ROUTES.HOME);
+    },
+  });
+
   return (
-    <form className="flex flex-col gap-2">
-      <UiTextField label="Email" />
-      <UiTextField label="Password" />
-      <UiButton variant="primary">Sign Up</UiButton>
+    <form
+      className="flex flex-col gap-2"
+      onSubmit={handleSubmit((data) => signUpMutation.mutate(data))}
+    >
+      <UiTextField
+        label="Email"
+        inputProps={{
+          type: 'email',
+          ...register('email', { required: true }),
+        }}
+      />
+      <UiTextField
+        label="Password"
+        inputProps={{
+          type: 'password',
+          ...register('password', { required: true }),
+        }}
+      />
+      <UiButton disabled={signUpMutation.isPending} variant="primary">
+        Sign Up
+      </UiButton>
     </form>
   );
 };
